84ca6455d8eaa4d12981232e0d919a39f575303b
=Irshad Ahamad <=irshadnisarkhan@gmail.com>
2020-04-14 23:11:21 +0530

    for now just default path has created

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get((req,res)=>{
+	res.send('I am default root');
+})
+
+app.listen(3000,'0.0.0.0',()=>{
+    console.log('server is runing on port 3000');
+})
